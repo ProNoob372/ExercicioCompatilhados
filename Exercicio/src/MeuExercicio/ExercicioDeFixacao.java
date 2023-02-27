e05edfddbdff9607372c2d1dac18fb925d36c686
USUARIO <USUARIO@LAPTOP-4TC5C3VJ>
2023-02-27 19:48:54 -0300

    exercicio de fixação

---
+++ Exercicio/src/MeuExercicio/ExercicioDeFixacao.java
@@ -0,0 +1,40 @@
+package MeuExercicio;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+
+public class ExercicioDeFixacao {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+
+		System.out.print("Enter account holder: ");
+		String holder = sc.next();
+
+		System.out.print("Is there na initial deposit (y/n)? ");
+		String answer = sc.nextLine();
+		if (answer.equals("Y")) {
+			double amount = sc.nextDouble();
+				
+		else {
+			double balance
+		}
+		
+		System.out.println("Account data: ");
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
---
+++ Exercicio/src/entities/Account.java
@@ -0,0 +1,23 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Account {
+	
+	public Integer number;
+	public String holder;
+	public double balance;
+	
+	
+	
+	public static void deposit(double amount) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+			
+		}
+		
+	}
+	
+	
+}
